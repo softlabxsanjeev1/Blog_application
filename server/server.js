6593d908fd4180715a414c936381c4f496ff898f
softlabxsanjeev1 <Sanjeevyaadav28520@gmail.com>
2024-06-25 07:31:14 +0530

    post feature integration in backend

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import morgan from 'morgan'
 import cookieParser from "cookie-parser";
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import postRoutes from './routes/post.route.js'
 
 
 
@@ -37,7 +38,7 @@ app.use("/test", (req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-// app.use('/api/post', postRoutes);
+app.use('/api/post', postRoutes);
 // app.use('/api/comment', commentRoutes);
 
 
